e35c0766644fa7f17bd77f85118ac6c1b76cdb07
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-13 13:30:44 -0600

    add imprts and useContext to access storeEnrolledCourses state

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,9 @@
 import logo from "./assets/logo.png";
+import React, { useContext } from "react";
+import { CourseContext } from "./App";
 
 export default function Header() {
+  const { storeEnrolledCourses } = useContext(CourseContext);
   return (
     <div className="header">
       <img src={logo} alt="logo" className="logo" />
