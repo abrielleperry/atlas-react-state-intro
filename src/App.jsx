85c32dfc808c994ae9c66c63dc677fc90c09d7a0
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-13 10:17:22 -0600

    create context object

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import SchoolCatalog from "./SchoolCatalog";
 import Header from "./Header";
 import ClassSchedule from "./ClassSchedule";
+import React from "react";
+
+export const Context = React.createContext();
 
 export default function App() {
   return (
