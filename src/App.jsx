bf123f5f349bee06a3df80dba71acc446b245c5e
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2025-01-13 11:08:57 -0600

    prev unfiinished commit message ...ClassCourse with context prover tag in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+/* personal notes:
+im going to think of context like a shared box that stores info all component have access to.
+*/
 import SchoolCatalog from "./SchoolCatalog";
 import Header from "./Header";
 import ClassSchedule from "./ClassSchedule";
